8c1a5d65d5332d4b895f03c12ffc5c2042d46e43
Sayantan Gain <sayantangain63@gmail.com>
2024-04-08 17:38:22 +0530

    structure

---
+++ frontend/app/(auth)/role/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Select Role</div>;
+};
+
+export default page;
---
+++ frontend/app/(main)/manager/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Manager home page</div>;
+};
+
+export default page;
