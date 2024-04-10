b0fc1739125782572bb08f60167d922dc2931a18
ph1ne4s <jainaviral2002@gmail.com>
2024-04-10 16:35:25 +0530

    jwt

--- backend/middlewares/jwt.js
+++ backend/middlewares/jwt.js
@@ -1,12 +1,11 @@
 // jwtMiddleware.js
 
 const jwt = require('jsonwebtoken');
-const { jwtSecret } = "ouhfsfnasdfnueafnouafouae"; // Import your JWT secret key
-
+const  jwtSecret  = "ouhfsfnasdfnueafnouafouae"; // Import your JWT secret key
+//console.log(jwtSecret);
 // Middleware to generate JWT token with user information including roles
 const generateToken = (user) => {
   const payload = {
-    id: user.id,
     email: user.email,
     roles: user.roles // Include user's roles in the JWT payload
   };
@@ -26,11 +25,12 @@ const jwtAuthMiddleware = (req, res, next) => {
 
     // Attach user data including roles to request for further use in routes
     req.user = decoded;
+    
 
     next();
   } catch (error) {
     return res.status(401).json({ error: 'Unauthorized: Invalid token' });
   }
 };
-
+//generateToken();
 module.exports = { generateToken, jwtAuthMiddleware };
